bbc0725d7146584501ae32aeceed03f64bb21dc5
vexplained <vexplained@users.noreply.github.com>
2026-02-13 15:18:06 +0100

    first commit

---
+++ testscript.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from typing import List
+
+VOLTAGE_SUPPLY = 20
+SENSORS = [16, 21]
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(VOLTAGE_SUPPLY, GPIO.OUT)
+    GPIO.output(VOLTAGE_SUPPLY, GPIO.LOW)
+    for sensor in SENSORS:
+        GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def read_sensor_state() -> List[bool]:
+    # create supply voltage
+    GPIO.output(VOLTAGE_SUPPLY, GPIO.HIGH)
+    sleep(0.1)
+    # read sensor values
+    readings = [False] * len(SENSORS)
+    for i in range(len(SENSORS)):
+        readings[i] = GPIO.input(SENSORS[i])
+    # turn off voltage supply
+    GPIO.output(VOLTAGE_SUPPLY, GPIO.LOW)
+    return readings
+
+if __name__ == "__main__":
+    setup()
+    print(read_sensor_state())
+    
+    GPIO.cleanup()
