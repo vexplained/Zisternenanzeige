b03780cf60d7b95e2b67eeb218067a8c16ebb9e2
vexplained <vexplained@users.noreply.github.com>
2026-02-19 03:27:14 +0100

    Autorun Script done

--- run_zisternenanzeige.sh
+++ run_zisternenanzeige.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 set -e
 filename="log_zisternenanzeige.log"
 if [ -e "$file" ]; then
     new_filename="$(date -r $filename +%Y%m%d_%H%M%S)"
     mv $filename $new_filename
 fi
-source "./Zisternenanzeige/.venv/bin/activate"
-python -u Zisternenanzeige/main.py
+source ".venv/bin/activate"
+python -u main.py
