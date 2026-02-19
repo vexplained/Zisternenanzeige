83b8ec8b3e9cb072828e7dc03239f8f66c2a138b
vexplained <vexplained@users.noreply.github.com>
2026-02-19 15:30:33 +0100

    Fix startup script logging bug

--- run_zisternenanzeige.sh
+++ run_zisternenanzeige.sh
@@ -2,9 +2,9 @@
 cd "$(dirname "$0")"
 set -e
 filename="log_zisternenanzeige.log"
-if [ -e "$file" ]; then
-    new_filename="$(date -r $filename +%Y%m%d_%H%M%S)"
-    mv $filename $new_filename
+if [ -e "$filename" ]; then
+    datestr="$(date -r $filename +%Y%m%d_%H%M%S)"
+    mv "$filename" "logs/log_$new_filename.log"
 fi
 source ".venv/bin/activate"
 python -u main.py
